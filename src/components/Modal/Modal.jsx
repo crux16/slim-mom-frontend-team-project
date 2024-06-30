04fb4624935ab0233d730453415e53444d517583
anggi <ortegacanillo76@gmail.com>
2024-06-30 09:22:09 +0800

    added layouts and folder structure

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,16 @@
+const Modal = ({ children, isOpen, onClose }) => {
+  if (!isOpen) {
+    return null;
+  }
+
+  return (
+    <div className="modal">
+      <div className="modal-content">
+        {children}
+        <button onClick={onClose}>Close</button>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
---
+++ src/pages/HealthHome/HealthHome.jsx
@@ -0,0 +1,10 @@
+const HealthHome = () => {
+  return (
+    <div className="container">
+      <h1>Home</h1>
+      <p>This is the Home page.</p>
+    </div>
+  );
+};
+
+export default HealthHome;
