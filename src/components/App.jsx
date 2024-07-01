df5966359319695e0080eea4db02787b04f91ec8
Paulo <pau.solanoy@gmail.com>
2024-07-01 21:02:37 +0800

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,48 @@
 import { SharedLayout } from './SharedLayout/SharedLayout';
 import { Route, Routes } from 'react-router-dom';
+import { RestrictedRoute } from './RestrictedRoute/RestrictedRoute';
+import { useEffect, lazy } from 'react';
+import { useDispatch } from 'react-redux';
+import { useAuth } from '../hooks/useAuth';
+import { refreshUser } from '../redux/auth/authOperations';
+
+const HomePage = lazy(() => import('../pages/HomePage/HomePage'));
+const LoginPage = lazy(() => import('../pages/LoginPage/LoginPage'));
+const RegisterPage = lazy(() => import('../pages/RegisterPage/RegisterPage'));
+
+export const App = () => {
+  const dispatch = useDispatch();
+  const { isRefreshing } = useAuth();
+
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch]);
+
+  return isRefreshing ? (
+    <b>Refreshing user...</b>
+  ) : (
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<HomePage />} />
+        <Route
+          path="/register"
+          element={
+            // <RegisterPage />
+            <RestrictedRoute redirectTo="/" component={<RegisterPage />} />
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            // <LoginPage />
+            <RestrictedRoute redirectTo="/" component={<LoginPage />} />
+          }
+        />
+      </Route>
+    </Routes>
+  );
+};
+
 // export const App = () => {
 //   return (
 //     <div
@@ -16,45 +59,3 @@ import { Route, Routes } from 'react-router-dom';
 //     </div>
 //   );
 // };
-
-export const App = () => {
-  //   const dispatch = useDispatch();
-  //   const { isRefreshing } = useAuth();
-
-  //   useEffect(() => {
-  //     dispatch(refreshUser());
-  //   }, [dispatch]);
-
-  return (
-    // isRefreshing ? (
-    //   <b>Refreshing user...</b>
-    // ) :
-    <Routes>
-      <Route path="/" element={<SharedLayout />}>
-        {/* /* <Route index element={<HomePage />} /> */}
-        {/* <Route
-          path="/register"
-          element={
-            <RestrictedRoute
-              redirectTo="/contacts"
-              component={<RegisterPage />}
-            />
-          }
-        /> */}
-        {/* <Route
-          path="/login"
-          element={
-            <RestrictedRoute redirectTo="/contacts" component={<LoginPage />} />
-          }
-        /> */}
-        {/* <Route
-          path="/contacts"
-          element={
-            <PrivateRoute redirectTo="/login" component={<ContactsPage />} />
-          }
-        /> */}
-        */ //{' '}
-      </Route>
-    </Routes>
-  );
-};
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,38 +1,64 @@
+import { Helmet, HelmetProvider } from 'react-helmet-async';
+import css from './HomePage.module.css';
+
 const HomePage = () => {
   return (
     <HelmetProvider>
       <Helmet>
-        <div>
-          <title>Calculate your daily calorie intake right now</title>
-          <form>
-            <div>
-              <label>
-                Height*
-                <input type="number" />
-              </label>
-              <label>
-                Desired Weight*
-                <input type="number" />
-              </label>
-            </div>
-            <div>
-              <label>
-                Age*
-                <input type="number" />
-              </label>
-              <label>
-                Blood type*
-                <input type="radio" />
-              </label>
-            </div>
-            <label>
-              Current weight*
-              <input type="number" />
-            </label>
-            <button>Start losing weight</button>
-          </form>
-        </div>
+        <title>Slim Moms</title>
       </Helmet>
+      <div className={css.container}>
+        <h1 className={css.title}>
+          Calculate your daily calorie intake right now
+        </h1>
+        <form>
+          <div>
+            <label htmlFor="">
+              <input type="number" />
+              <span className={css.name}>Height*</span>
+            </label>
+            <label htmlFor="">
+              <input type="number" />
+              <span className={css.name}>Desired Weight*</span>
+            </label>
+          </div>
+          <div>
+            <label htmlFor="">
+              <input type="number" />
+              <span className={css.name}>Age*</span>
+            </label>
+            <label htmlFor="">
+              <input type="text" />
+              <span className={css.name}>Blood Type*</span>
+            </label>
+          </div>
+          <div>
+            <label htmlFor="">
+              <input type="number" />
+              <span className={css.name}>Current Weight*</span>
+            </label>
+            <label htmlFor="">
+              <input type="radio" />
+              <span>1</span>
+            </label>
+            <label htmlFor="">
+              <input type="radio" />
+              <span>2</span>
+            </label>
+            <label htmlFor="">
+              <input type="radio" />
+              <span>3</span>
+            </label>
+            <label htmlFor="">
+              <input type="radio" />
+              <span>4</span>
+            </label>
+          </div>
+          <button type="submit" className={css.button}>
+            Start losing weight
+          </button>
+        </form>
+      </div>
     </HelmetProvider>
   );
 };
