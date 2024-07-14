97a7e10d31cb020a6a2a590b1f1cf7dc4b62c1c5
nyotnoti08 <ynoalcala08@gmail.com>
2024-07-14 22:50:02 +0800

    Update Modal-patch 1

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,60 +1,37 @@
-// import {
-//   Box,
-//   Divider,
-//   Link,
-//   Modal,
-//   ModalBody,
-//   ModalCloseButton,
-//   ModalContent,
-//   ModalFooter,
-//   ModalHeader,
-//   Text,
-// } from '@chakra-ui/react';
-// import GrayBar from '../GrayBar/GrayBar';
-
-// import logo from 'assets/images/logo.png';
-
-// const Modal = ({ children, isOpen, onClose }) => {
-//   if (!isOpen) {
-//     return null;
-//   }
-
-//   return (
-//     <div className="modal">
-//       <div className="modal-content">
-//         {children}
-//         <button onClick={onClose}>Close</button>
-//       </div>
-//     </div>
-//   );
-// };
-
-
 import css from './Modal.module.css';
-// import { MdClose } from 'react-icons/md';
 import { useEffect } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
-// import {
-//   getDailyRate,
-//   getIsLoading,
-//   getNotAllowedProducts,
-// } from '../../redux/dailyRate/dailyRateSelectors';
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+// import { useState } from 'react';
+// import CalculatorForm from 'components/CalculatorForm/CalculatorForm';
 
-function Modal({ closeModal, openModal }) {
-  // const dailyRate = useSelector(getDailyRate);
-  // const isLoading = useSelector(getIsLoading);
-  // const notAllowedProducts = useSelector(getNotAllowedProducts);
-  // const navigate = useNavigate();
-  // const dispatch = useDispatch();
-  // const handleClick = () => {
-  //   navigate('/registration');
-  //   dispatch(getDailyRate(null));
-  // };
+
+import {
+  getCalculatorDailyRate,
+} from 'redux/user/userSelectors';
+
+function Modal({ closeModal }) {
+  const dailyRate = useSelector(getCalculatorDailyRate);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/RegisterPage');
+    dispatch(getCalculatorDailyRate(null));
+  };
+  // const [age, setAge] = useState(0);
+  // const [height, setHeight] = useState(0);
+  // const [currentWeight, setCurrentWeight] = useState(0);
+  // const [desiredWeight, setDesiredWeight] = useState(0);
+  // const [dailyRate, setDailyRate] = useState('Computing...');
+
   useEffect(() => {
     const handleKeyDown = e => {
       if (e.code === 'Escape');
       closeModal();
     };
+
 
     window.addEventListener('keydown', handleKeyDown);
     const body = document.querySelector('body');
@@ -67,19 +44,27 @@ function Modal({ closeModal, openModal }) {
     
   }, [closeModal]);
 
+    // const calculateDailyRate = () => {
+    // const bmr = 10 * currentWeight + 6.25 * height - 5 * age - 161;
+
+    // const activityFactor = 1.2;
+
+    // const calories = Math.round(bmr * activityFactor);
+    // setDailyRate(calories);
+    // };
+
   return (
     <div>
       <div className={css.modalBackground} onClick={closeModal}>
       </div>
       <div className={css.modalContainer}>
-        {/* <button className={css.modalCloseBtn} onClick={closeModal}>
-          <MdClose />
-        </button> */}
         <h1 className={css.modalHeader}>
           Your recommended daily calorie intake is
         </h1>
+        {/* <p className={css.modalTotalCalories}><span className={css.modalCaloriesLabel}>KCal</span>
+        </p> */}
         <p className={css.modalTotalCalories}>
-          X <span className={css.modalCaloriesLabel}>KCal</span>
+        {dailyRate} <span className={css.modalCaloriesLabel}>KCal</span>
         </p>
         <div className={css.modalProhibitedFoodContainer}>
           <h2 className={css.modalProhibitedFoodTitle}>
@@ -87,17 +72,11 @@ function Modal({ closeModal, openModal }) {
           </h2>
           <ol className={css.modalProhibitedFoodList}>
 
-            {/* {notAllowedProducts.map((item, index) => (
-              <li className={css.modalProhibitedFoodItem} key={index}>
-                {index + 1}. {item}
-              </li>
-            ))} */}
-
             <li className={css.modalProhibitedFoodItem}>List Number 1</li>
           </ol>
         </div>
-        <button className={css.modalStartBtn}>Start Losing Weight</button>
-
+        {/* <button onClick = {calculateDailyRate} className={css.modalStartBtn} >Start Losing Weight</button> */}
+        <button onClick={handleClick} className={css.modalStartBtn} >Start Losing Weight</button>
       </div>
     </div>
   );
