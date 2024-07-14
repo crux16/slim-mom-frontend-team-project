b9b6521c8fcf6fef6ed5777f36a96a7dc6bb0f4d
anggi <ortegacanillo76@gmail.com>
2024-07-14 19:24:07 +0800

    added toast and remove state token on register in authSlice.js[22-23]

--- src/redux/auth/authOperations.js
+++ src/redux/auth/authOperations.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
+import { toast } from 'react-toastify';
 
-axios.defaults.baseURL = 'https://slim-mom-backend-team-project-ik9a.onrender.com/';
+axios.defaults.baseURL =
+  'https://slim-mom-backend-team-project-ik9a.onrender.com/';
 // axios.defaults.baseURL = 'http://localhost:3000';
 
 const setAuthHeader = token => {
@@ -16,10 +18,16 @@ export const register = createAsyncThunk(
   'auth/register',
   async ({ name, email, password }, thunkAPI) => {
     try {
-      const res = await axios.post('api/users/signup', { name, email, password });
+      const res = await axios.post('api/users/signup', {
+        name,
+        email,
+        password,
+      });
+      toast.success('Registration successful');
       setAuthHeader(res.data.token);
       return res.data;
     } catch (error) {
+      toast.error('Registration failed');
       return thunkAPI.rejectWithValue(error.message);
     }
   }
@@ -30,23 +38,29 @@ export const logIn = createAsyncThunk(
   async ({ email, password }, thunkAPI) => {
     try {
       const res = await axios.post('api/users/login', { email, password });
-      setAuthHeader(res.data.token);
+      toast.success('Login successful');
+      setAuthHeader(res.data.data.token);
       return res.data;
     } catch (error) {
+      toast.error('Login failed');
       return thunkAPI.rejectWithValue(error.message);
     }
   }
 );
 
-export const logOut = createAsyncThunk('api/auth/logout', async (_, thunkAPI) => {
-  try {
-    await axios.post('api/users/logout');
-    // After a successful logout, remove the token from the HTTP header
-    clearAuthHeader();
-  } catch (error) {
-    return thunkAPI.rejectWithValue(error.message);
+export const logOut = createAsyncThunk(
+  'api/auth/logout',
+  async (_, thunkAPI) => {
+    try {
+      await axios.post('api/users/logout');
+      toast.success('Logout successful');
+      // After a successful logout, remove the token from the HTTP header
+      clearAuthHeader();
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
   }
-});
+);
 
 export const refreshUser = createAsyncThunk(
   'auth/refresh',
